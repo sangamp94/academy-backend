ce41ff0439ae9cadaf1c12c1c79a897e11927bd8
sangamp94 <167419902+sangamp94@users.noreply.github.com>
2026-01-08 18:19:41 +0530

    Create Admin.js

---
+++ api/models/Admin.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const AdminSchema = new mongoose.Schema({
+  academyName: { type: String, required: true },
+  adminName: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now }
+});
+
+export default mongoose.models.Admin ||
+  mongoose.model("Admin", AdminSchema);
